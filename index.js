4b0253bdc1de802410cf64fa0ff856d86854329e
David Carvajal <david.carvajal@gruposantander.com>
2021-03-05 09:16:15 +0100

    added output

--- index.js
+++ index.js
@@ -4,14 +4,12 @@ const semver = require('semver')
 const fs = require('fs')
 
 try {
-  const current_version = fs.readFileSync('./gradle.properties', {encoding:'utf8', flag:'r'})
+  const old_version = fs.readFileSync('./gradle.properties', {encoding:'utf8', flag:'r'})
   const type = core.getInput('type')
-  let new_version = semver.inc(current_version, type)
+  let new_version = semver.inc(old_version, type)
   fs.writeFileSync('./gradle.properties', new_version, {encoding:'utf8', flag:'w'})
-  
-  console.log("current_version"+current_version)
-  console.log("new_version"+new_version)
-
+  core.setOutput('old_version', old_version)
+  core.setOutput('new_version', new_version)
 } catch (error) {
   core.setFailed(error.message);
 }
